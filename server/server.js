467f020cca1f370d46e1a14e6f80e106ae8d8a07
Priyangshu <shomepriyangshu16@gmail.com>
2025-02-19 23:49:41 +0530

    improved server consistency

--- server/server.js
+++ server/server.js
@@ -2,38 +2,54 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
-import redis from "redis";
+import { createClient } from "redis";
+
+import authRoutes from "./routes/auth.routes.js"
+import blogRoutes from "./routes/blog.routes.js"
 
 dotenv.config();
 const server = express();
 
-const redisClient = redis.createClient();
-
+const redisClient = createClient();
 redisClient.on('error', (err) => console.error(`Redis error: ${err}`));
 
-const cacheMiddleware = (req, res, next) => {
-    const key = req.originalUrl;
-    redisClient.get(key, (err, data) => {
-        if(err) throw err;
-        if(data) return res.json(JSON.parse(data));
-        next();
-    })
-};
+const cacheMiddleware = async (req, res, next) => {
+    try {
+        const key = req.originalUrl;
+        const data = await redisClient.get(key);
 
+        if(data) {
+            return res.json(JSON.parse(data));
+        }
+        next();
+    } catch (error) {
+        console.error("Redis error:", error);
+        next();
+    }
+};
 
 server.use(cors());
 server.use(express.json());
-
 server.use('/auth', authRoutes);
 server.use('/blog', blogRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('DB connected'))
-    .catch(error => console.error(`DB Connection failed: ${error}`));
-    
+const PORT = process.env.PORT || 3003;
+
+const startServer = async () => {
+    try {
+        await redisClient.connect();
+        console.log("✅ Connected to Redis");
+
+        await mongoose.connect(process.env.MONGO_URI)
+            .then(() => console.log('DB connected'))
+            .catch(error => console.error(`DB Connection failed: ${error}`));
+
+        server.listen(PORT, () => console.log(`✅ Server connected on port ${PORT}`)); 
+    } catch (error) {
+        console.error("❌ Error starting the server:", error);   
+    }
+};
+
+startServer();
 export { redisClient, cacheMiddleware}
 
-const PORT = process.env.PORT || 3003;
-server.listen(PORT, () => {
-    console.log(`Server connected on port ${PORT}`);
-});
