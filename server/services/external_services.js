dc402c0461f4f5395a57490e50f768e8b276c705
Priyangshu <shomepriyangshu16@gmail.com>
2025-03-05 23:27:44 +0530

    resolving external sources

--- server/services/external_services.js
+++ server/services/external_services.js
@@ -1,28 +1,35 @@
 import axios from "axios";
+import { redisClient } from "../server.js";
 
 export const fetchExternalData = async () => {
+  const cacheKey = "news_data";
+
   try {
-    const options = {
-      method: "GET",
-      url: "https://real-time-news-data.p.rapidapi.com/top-headlines",
+    const cacheData = await redisClient.get(cacheKey);
+    if (cacheData) {
+      console.log("Serving data from cache");
+      return JSON.parse(cacheData);
+    }
+
+    const response = await axios.get("https://api.mediastack.com/news", { 
       params: {
-        limit: "500",
-        country: "IN",
-        lang: "en",
+        access_key: process.env.MEDIASTACK_API_KEY, 
+        country: "in" 
       },
-      headers: {
-        "x-rapidapi-key": "9f91d6b49cmsh484dde7cc301f44p139a86jsn2460d88fdeb8",
-        "x-rapidapi-host": "real-time-news-data.p.rapidapi.com",
-      },
-    };
+    });
+    response = JSON.stringify(response)
+    console.log(`response: ${response.data}`)
 
-    const response = await axios.request(options);
-
-    if(response.data && response.data.data) {
-        return response.data.data || [];
+    if (response.data?.data) {
+      await redisClient.setEx(
+        cacheKey,
+        600,
+        JSON.stringify(response.data.data)
+      );
+      return response.data.data || [];
     } else {
-        console.error("Unexpected API response format:", response.data)
-        return [];
+      console.error("Unexpected API response format:", response.data);
+      return [];
     }
   } catch (error) {
     console.error("Error fetching external data:", error.message);
